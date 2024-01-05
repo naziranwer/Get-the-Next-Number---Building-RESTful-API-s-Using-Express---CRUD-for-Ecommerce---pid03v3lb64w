90b0cdf8d03ea05be8d0b9aef411a96c279006c0
Nazir <anwernazir674@gmail.com>
2024-01-06 01:29:52 +0530

    update

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ app.use(express.json());
 // Write a GET Request to get the next number from the input 'num'.
 // Endpoint : /api/get-next-num
 // Return the response as {message : , status: }
+app.get("/api/:num",(req,res)=>{
+       let {num}=req.params;
+       num*=1;
+       if(!isNaN(num)){
+          res.send({'message':num+1, "status":"success"});
+       }
+       else{
+        res.send({'message':"error", "status":"failure"});
+       }
+})
 
 module.exports = app;
